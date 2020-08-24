1a65ed773e3ef7bb290aa00979574466c5dc0662
wrui <wrui@bw30.com>
2020-08-24 11:37:24 +0800

    wrui

--- src/main/java/personal/wrui/test/mysql/jpa/pojo/dao/PersonalEntity.java
+++ src/main/java/personal/wrui/test/mysql/jpa/pojo/dao/PersonalEntity.java
@@ -3,6 +3,8 @@ package personal.wrui.test.mysql.jpa.pojo.dao;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +14,7 @@ import javax.persistence.Table;
 public class PersonalEntity implements Serializable{
 
 	
-	@Id
+	@Id@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String name;
